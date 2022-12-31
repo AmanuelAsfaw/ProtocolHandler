9b13920b67d068b989138bcd2952481583fb4335
alemseged_t <lealemseged@gmail.com>
2022-12-31 16:40:28 +0300

    First Commit

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string[] argsList;
+        private string str_text = "";
+        public Form1(string[] args)
+        {
+            argsList = args;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(string arg in argsList)
+            {
+                str_text = str_text + arg;
+            }
+            label1.Text = str_text;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            //RegisterMyProtocol("");
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string[] args = Environment.GetCommandLineArgs();
+            Application.Run(new Form1(args));
+        }
+        static void RegisterMyProtocol(string myAppPath)  //myAppPath = full path to your application
+        {
+            RegistryKey key = Registry.ClassesRoot.OpenSubKey("myApp");  //open myApp protocol's subkey
+
+            if (key == null)  //if the protocol is not registered yet...we register it
+            {
+                key = Registry.ClassesRoot.CreateSubKey("myApp");
+                key.SetValue(string.Empty, "URL: myApp Protocol");
+                key.SetValue("URL Protocol", string.Empty);
+
+                key = key.CreateSubKey(@"shell\open\command");
+                key.SetValue(string.Empty, myAppPath + " " + "%1");
+                //%1 represents the argument - this tells windows to open this program with an argument / parameter
+            }
+
+            key.Close();
+        }
+    }
+}
+
+
+
+
+// related links
+// https://codingvision.net/c-register-a-url-protocol
+// customurl://?param1=xy&param2=xy
+// https://stackoverflow.com/questions/80650/how-do-i-register-a-custom-url-protocol-in-windows
+// https://www.youtube.com/watch?v=bJw0eGfM1ZU
+// https://github.com/r-aghaei/UrlSchemeSample
+// https://github.com/madskristensen/ProtocolHandlerSample
